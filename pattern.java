b2414bd67721efdd48d9d41d5a3cca0e35a3b873
kashyapshankar0@gmal.com <kashypshankar0@gmail.com>
2018-12-15 11:23:38 +0530

    new file added pattern.java

---
+++ pattern.java
@@ -0,0 +1,17 @@
+
+
+public class pattern {
+
+	public static void main(String[] args) {
+	
+		/*int n = 0;
+		int a=5;
+		for(int i=0;i<a;i++) {
+			i=n;
+			for(int j=0;j<=i;j++) {
+				System.out.print(n);
+				n=n+a-j;*/
+				System.out.println("hello  please dont mind");
+
+	}
+}	
